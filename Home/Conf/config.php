03cb8f819aed39f2f12cacf2e8ad6f53d157cc2b
wudao <wudao@localhost>
2013-01-31 03:16:54 +0800

    ?????

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -5,8 +5,8 @@ return array(
 	
 	'LANG_SWITCH_ON' => true,
 	'LANG_AUTO_DETECT' => true, // 自动侦测语言 开启多语言功能后有效
-	'DEFAULT_LANG' => 'zh-cn', // 默认语言
-	'LANG_LIST'        => 'zh-cn,en-us', // 允许切换的语言列表 用逗号分隔
+	'DEFAULT_LANG' => 'en-us', // 默认语言
+	'LANG_LIST'        => 'zh-cn, en-us', // 允许切换的语言列表 用逗号分隔
 	'VAR_LANGUAGE'     => 'l', // 默认语言切换变量
 	
 	'DB_TYPE' => 'pdo',
