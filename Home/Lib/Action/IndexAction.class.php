16652b954795d14de1388dda7d5b66a4893290b9
Kyle <Kyle@localhost>
2013-02-02 23:24:23 +0800

    ??????

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -1,10 +1,17 @@
 <?php
 class IndexAction extends Action {
     public function index(){
+		//近期活动
 		$tActivities = M('activities');
 		$lang = L('lang');
 		$activities = $tActivities->field('title,place,date_start,date_end')->where("lang=\"{$lang}\"")->order('date_end DESC')->limit(3)->select();
 		$this->assign('activities', $activities);
+		
+		//每月之星
+		$tStar = M('star');
+		$lang = L('lang');
+		$stars = $tStar->where("lang=\"{$lang}\"")->order('id DESC')->limit(3)->select();
+		$this->assign('stars', $stars);
 		$this->display();
     }
 }
