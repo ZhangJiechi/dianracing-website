8f3d723cd9b96702ea03d821cc1862870be5557a
Kyle <admin@hk1229.cn>
2013-02-20 21:10:28 +0800

    Member & Star in Index

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -3,26 +3,37 @@ include(APP_PATH.'/Lib/Action/GlobalAction.class.php');
 
 class IndexAction extends GlobalAction {
     public function index(){
+		//语言
+		$lang = L('lang');
+		
 		//近期活动
 		$tActivities = M('activities');
-		$lang = L('lang');
-		$activities = $tActivities->field('title,place,date_start,date_end')->where("lang=\"{$lang}\"")->order('date_end DESC')->limit(3)->select();
-		$this->assign('activities', $activities);
+		$ret = $tActivities->field('title,place,date_start,date_end')->where("lang=\"{$lang}\"")->order('date_end DESC')->limit(3)->select();
+		$this->assign('activities', $ret);
+		unset($tActivities);
 		
 		//每月之星
 		$tStar = M('star');
-		$lang = L('lang');
-		$stars = $tStar->where("lang=\"{$lang}\"")->order('id DESC')->limit(3)->select();
-		$this->assign('stars', $stars);
+		$ret = $tStar->where("lang=\"{$lang}\"")->order('id DESC')->limit(3)->select();
+		$this->assign('stars', $ret);
+		unset($tStar);
 		
-		//职员
+		//底部职员
 		$tStaff = M('staff');
-		$lang = L('lang');
-		$staffs = $tStaff->where("lang=\"{$lang}\"")->order('queue ASC')->limit(4)->select();
-		$this->assign('staffs', $staffs);
+		$ret = $tStaff->where("lang=\"{$lang}\"")->order('queue ASC')->limit(4)->select();
+		$this->assign('staffs', $ret);
+		unset($tStaff);
 		
-		
-	
+		//member
+		$tMember = M('member');
+		$ret = $tMember->field('face,text')->where("lang=\"{$lang}\" AND gtype=0")->find();
+		$this->assign('member', $ret);
+		unset($tMember);
+		//team
+		$tTeam = M('group');
+		$ret = $tTeam->field('gtype,name')->where("lang=\"{$lang}\" AND gtype>0 AND children<>'-'")->order('gtype ASC')->select();
+		$this->assign('teams', $ret);
+		unset($tTeam);
 		
 		//显示
 		$this->assign('selectedTab', 'index');
